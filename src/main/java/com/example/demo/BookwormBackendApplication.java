518c5b5912910ec68e71bbd2c5e7836adf47b6fb
rohan-1112 <rohankalaskar1112@gmail.com>
2024-08-06 18:14:17 +0530

    ready to start backend project

---
+++ src/main/java/com/example/demo/BookwormBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwormBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwormBackendApplication.class, args);
+	}
+
+}
